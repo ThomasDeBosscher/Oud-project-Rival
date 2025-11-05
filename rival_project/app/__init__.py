39cbdf86bcd12db194899cac2e0254f7f2fe1807
LeRustique5 <Thomas.DeBosscher@ugent.be>
2025-11-05 17:06:46 +0100

    f

---
+++ rival_project/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    from app import routes
+    routes.init_app(app)
+
+    return app
